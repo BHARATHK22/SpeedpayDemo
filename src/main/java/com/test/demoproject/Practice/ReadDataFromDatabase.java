a19d4525ce474dc5988eca19bcbd2307534e17d5
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2021-03-20 21:43:54 +0530

    updated

--- src/main/java/com/test/demoproject/Practice/ReadDataFromDatabase.java
+++ src/main/java/com/test/demoproject/Practice/ReadDataFromDatabase.java
@@ -4,25 +4,48 @@ public class ReadDataFromDatabase {
 
 	public static void main(String [] args) {
 		  String dataBaseName="GmFinance";
-	 
+		  Connection connection;
+		  ResultSet resultSet;
+		  String creditAccount=null;
 		 try{  
 			 Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-             Connection connection= DriverManager.getConnection("jdbc:ucanaccess://E:\\Tables\\ClientDatabase.accdb");
+             connection= DriverManager.getConnection("jdbc:ucanaccess://E:\\Tables\\ClientDatabase.accdb");
              System.out.println("Connected Successfully"); 
              PreparedStatement preparedStatement=connection.prepareStatement("select top 1 * from "+dataBaseName+" order by rnd()");
              
-             ResultSet resultSet=preparedStatement.executeQuery();
+             // Get Random Credit Account
+             resultSet=preparedStatement.executeQuery();
              while(resultSet.next()){
-                  String creditAccount=resultSet.getString("Credit_Account");
+                   creditAccount=resultSet.getString("Credit_Account");
                   String phone=resultSet.getString("Phone");
                  
                   System.out.println("creditAccount::"+creditAccount);
-                  System.out.println("creditAccount::"+phone);
+                  System.out.println("Phone Number ::"+phone);
+                 
              }
+             
+             //Update phone number for a particular credit account
+             String newPhoneNumber= "5555555555551";
+             preparedStatement=connection.prepareStatement("update "+dataBaseName+" set Phone="+newPhoneNumber+" where Credit_Account="+creditAccount);
+             preparedStatement.executeUpdate();
+             System.out.println("data updated successfully\n");
+             
+             
+             //Retrieve particular credit account
+             preparedStatement=connection.prepareStatement("select * from "+dataBaseName+" where Credit_Account="+creditAccount);
+             resultSet=preparedStatement.executeQuery();
+             while(resultSet.next()){
+            	 creditAccount=resultSet.getString("Credit_Account");
+                 String phone=resultSet.getString("Phone");
+                
+                 System.out.println("creditAccount       ::"+creditAccount);
+                 System.out.println("Updated Phone NUmber::"+phone);
+             }
+		
+		 }catch(Exception e){
+			e.printStackTrace();
+		}
 		  
-		}catch(Exception ee){
-			System.out.println(ee);}  
-		  
+	
 	}
-
 }
